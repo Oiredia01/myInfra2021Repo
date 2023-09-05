103c1bfede50114985c623e51fe71cd2d4bca39d
Oiredia01 <140750041+Oiredia01@users.noreply.github.com>
2023-09-05 15:20:08 -0500

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,6 +1,6 @@
 variable "aws_region" {
        description = "The AWS region to create things in." 
-       default     = "us-east-1" 
+       default     = "us-east-2" 
 }
 
 variable "key_name" { 
@@ -20,7 +20,7 @@ variable "security_group" {
 
 variable "tag_name" { 
     description = "Tag Name of for Ec2 instance" 
-    default     = "my-ec2-instance" 
+    default     = "my-terraform-ec2-instance" 
 } 
 variable "ami_id" { 
     description = "AMI for Ubuntu Ec2 instance" 
